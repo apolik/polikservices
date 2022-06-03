9427b465312a659846f26ee3e0f72317207c12f7
lodjdheheu <lodjdheheu@gmail.com>
2022-06-03 21:56:36 +0300

    Api Gateway With Spring Cloud Gateway section commit

---
+++ apigw/src/main/java/org/polik/apigw/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package org.polik.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by Polik on 6/3/2022
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
